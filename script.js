b5f0bf4aaec01404a0ac91cfd374af9360b50c76
138420059930 <arshabank1384@gmail.com>
2025-03-21 08:49:46 +0100

    rotate

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function klikk() {
-    const dropdownElm = document.querySelector(".dropdown-content")
-    if (dropdownElm.style.display == "flex") {
-        dropdownElm.style.display = "none"
+    const dropdownElm = document.querySelector(".dropdown-content");
+    const caretIcon = document.querySelector(".fa-caret-up");
+
+    // Toggle dropdown visibility
+    if (dropdownElm.style.display === "flex") {
+        dropdownElm.style.display = "none";
     } else {
-        dropdownElm.style.display = "flex"
+        dropdownElm.style.display = "flex";
     }
+
+    // Toggle rotate class on the caret icon
+    caretIcon.classList.toggle("rotate");
 }
