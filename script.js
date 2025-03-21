5a3217a8728ab7f06f9f18d9c3b30c1e2b9699d2
138420059930 <arshabank1384@gmail.com>
2025-03-21 09:09:09 +0100

    improved the navbar dropdown click

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-function klikk() {
-    const dropdownElm = document.querySelector(".dropdown-content");
-    const caretIcon = document.querySelector(".fa-caret-up");
+const dropdownElm = document.querySelector(".dropdown-content")
+const caretIcon = document.querySelector(".fa-caret-up")
 
+const dropDownElm = document.querySelector("nav .dropbtn")
+dropDownElm.addEventListener("click", dropdownKlikk)
+
+function dropdownKlikk(event) {
     // Toggle dropdown visibility
     if (dropdownElm.style.display === "flex") {
-        dropdownElm.style.display = "none";
+        dropdownElm.style.display = "none"
     } else {
-        dropdownElm.style.display = "flex";
+        dropdownElm.style.display = "flex"
     }
 
     // Toggle rotate class on the caret icon
-    caretIcon.classList.toggle("rotate");
+    caretIcon.classList.toggle("rotate")
+}
+
+function documentKlikk(event) {
+    // Hide the navbar dropdown
+    dropdownElm.style.display = "none"
 }
